3e75b6c34a65e36f4cf341151a5cb83dc76dbdb9
Avi Romanoff <aroman@users.noreply.github.com>
2014-05-08 20:25:13 -0400

    Update package.js

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
 	summary: "chirag04/mail-listener2 packaged for Meteor."
 });
 
-Npm.depends({ 'mail-listener2': '0.1.1' });
+Npm.depends({ 'mail-listener2': '0.1.3' });
 
 Package.on_use(function (api) {
 	api.add_files('mail_listener2.js', 'server');
